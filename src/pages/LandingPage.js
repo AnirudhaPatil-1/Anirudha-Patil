1cf4e6f2b6bb56cdbef6bb0edf38b83337801036
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-08-24 23:43:33 +0530

    landing page created

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import {Navigate} from 'react-router-dom'
+import Navbar from '../components/Navbar';
+import Content from "../components/Content";
+
+const LandingPage = () => {
+  return (
+
+    <>
+    <Navbar/>
+    <Content/>
+    </>
+  )
+}
+
+export default LandingPage;
